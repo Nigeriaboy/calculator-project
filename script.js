67d3982a6b7bb5a203eea159d930b02d48010e88
Nigeriaboy <abdurahmonabdullah247@gmail.com>
2025-06-01 19:59:52 +0100

    Create calculation logic and UI
    Done with add, subtract, divide, multiply,isNumber(function that checks if a passed input is a number), toTenDecimalPlaces function
    and designing the user interface to the interface of Samsung s9+ calculator

---
+++ script.js
@@ -0,0 +1,104 @@
+function add (a, b) {
+    return toTenDecimalPlaces(a + b);    
+}
+
+function subtract (a, b) {
+    return toTenDecimalPlaces(a - b);    
+}
+
+function multiply (a, b) {
+    return toTenDecimalPlaces(a * b);    
+}
+
+function divide (numerator, denominator) {
+    if (denominator === 0) {
+        return "Cannot divide by zero";
+    }
+    return toTenDecimalPlaces(numerator / denominator);
+}
+
+function isNumber(value){
+    let num = Number(value); // convert value to a number
+    return !isNaN(num); // check if the value is a valid number
+}
+
+function toTenDecimalPlaces(value) {
+    return Number(value.toFixed(10)); // return the value rounded to 10 decimal places
+}
+
+// this function handles the operation/calculation based on the operator passed
+function operate (operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case 'x':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
+
+const  expressionDisplay = document.querySelector('#expression-display');
+const  resultDisplay = document.querySelector('#result-display');
+const backspaceBtn = document.querySelector('#backspace-btn');
+const btnSection = document.querySelectorAll('#btn-section button');
+
+const operatorsArray = ['+', '-', 'x', '/'];
+
+expressionDisplay.textContent = '';
+resultDisplay.textContent = '';
+
+let firstOperand = '';
+let secondOperand = '';
+let operator = undefined;
+let result = '';
+
+// This section handles each button click event
+btnSection.forEach((button) => {
+    button.addEventListener('click', () => {
+        const buttonText = button.textContent;
+
+        /** check if the button clicked is a dot so that the variable can be assigned 0.
+        if (buttonText === '.'){
+            if (firstOperand === undefined){
+                firstOperand = 0.;
+                expressionDisplay.textContent += firstOperand;
+            }
+        } */
+
+        
+        if (isNumber(buttonText) && operator === undefined){
+            firstOperand += buttonText;
+            expressionDisplay.textContent += buttonText;
+        }
+        else if ((firstOperand !== '' && operator !== undefined) && isNumber(buttonText)){
+            secondOperand += buttonText;
+            expressionDisplay.textContent += buttonText;
+            // display the direct result after the second operand is entered
+            result = operate(operator, Number(firstOperand), Number(secondOperand));
+            resultDisplay.textContent = result;
+        }
+        else if (operatorsArray.includes(buttonText) && (firstOperand !== '' && secondOperand === '')) {
+            if (operator !== undefined) {
+                expressionDisplay.textContent = expressionDisplay.textContent.slice(0, -2); // remove the previous operator
+            }
+
+            operator = buttonText;
+            expressionDisplay.textContent += ` ${operator} `;
+        }
+
+     if (buttonText === '=' && firstOperand !== '' && secondOperand !== '' && operator !== undefined) {
+            // reset the operands and operator for the next calculation
+            firstOperand = result; // store the result as the first operand for further calculations
+            secondOperand = '';
+            operator = undefined;
+            expressionDisplay.textContent = `${firstOperand}`;
+
+        }
+
+    });
+})
+
+
